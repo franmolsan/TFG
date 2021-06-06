4d603ed093efc0fa945c1a7a01d7ad15a2988033
franmolsan <56502397+franmolsan@users.noreply.github.com>
2021-06-06 14:53:53 +0200

    clicks registrados

--- API/app.js
+++ API/app.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
-const BodyParser = require("body-parser");
+// const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const CONNECTION_URL = process.env.MONGO_CONNECTION_URL;
 const DATABASE_NAME = process.env.DATABASE_NAME;
 
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) 
 
 app.listen(port, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
@@ -42,20 +45,22 @@ app.get('/products', (req,res) => {
     res.json(products)
 })
 
-app.get('/products', (req,res) => {
-    const products = [
-        {
-            id:1,
-            name: "hammer",
-        },
-        {
-            id:2,
-            name: "screwdriver",
-        },
-        {
-            id:3,
-            name:"wrench",
-        },
-    ];
-    res.json(products)
-})
+app.post("/button-click", (request, response) => {
+
+    var clickType = request.body.clickType;
+
+    console.log(clickType)
+    
+    collectionButtonClicks.updateOne({ "serialNumber":request.body.serialNumber }, {"$inc": { [clickType] : 1} }, (error, result) => {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        if (result == null) {
+            response.send("No se pudo incrementar correctamente");
+        }
+        else {
+            response.send("Incremento correcto");
+        }
+    });
+
+});
